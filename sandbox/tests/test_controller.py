aaaddbbfdea985a41eeb182c3beb985a63f97171
Christopher Sams <csams@redhat.com>
2020-07-17 16:41:24 -0500

    RunnerAdapterProxy tests.
    Signed-off-by: Christopher Sams <csams@redhat.com>

--- sandbox/tests/test_controller.py
+++ sandbox/tests/test_controller.py
@@ -1,21 +1,6 @@
-from collections import deque
-import dill
-
 from sandbox.controller import Controller
 from sandbox.protocol import Setup, Process, Stop
-
-
-class IO:
-    def __init__(self, inbuf):
-        self.inbuf = deque(inbuf)
-        self.output = []
-
-    def recv(self):
-        cmd, payload = self.inbuf.popleft()
-        return (cmd, dill.dumps(payload))
-
-    def send(self, payload):
-        self.output.append(payload)
+from sandbox.tests import IO
 
 
 def handle_setup(payload):
